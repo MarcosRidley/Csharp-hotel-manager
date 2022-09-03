06b3cd63e566beb8aa98e731bd35a17b88c3c318
MarcosRidley <marcosridley@hotmail.com>
2022-09-03 12:37:23 -0300

    Add project files.

---
+++ HotelManager/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace HotelManager {
+    
+    class Inquilino {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public int Quarto { get; set; }
+
+        public Inquilino(string nome, string email, int quarto) {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString() {
+            return "Quarto " + Quarto + ": " + Nome + ", " + Email;
+        }
+    }
+
+    class Program {
+        static void Main() {
+            int occupants = int.Parse(Console.ReadLine());
+            Inquilino[] inquilinos = new Inquilino[10];
+
+            for (int i = 0; i < occupants; i++) {
+                string name = Console.ReadLine();
+                string email = Console.ReadLine();
+                int quarto = int.Parse(Console.ReadLine());
+                inquilinos[quarto] = new Inquilino(name ?? "nome não informado", email ?? "email não informado", quarto);
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for(int i = 0; i < occupants; i++) {
+                Console.WriteLine(inquilinos[i]);
+            }
+        }
+    }
+}
